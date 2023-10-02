09d06b88535564ec1c201239c86f3c3b50df27bd
Jamie Harris <jamiedodgerification12345@gmail.com>
2023-10-02 16:04:49 +0100

    feat(setup): add 2022 day 1 solutions

--- Advent.Of.Code.Helper/Advent.Of.Code.Helper.Test/UnitTest1.cs
+++
@@ -1,8 +0,0 @@
-namespace Advent.Of.Code.Helper.Test
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1() { }
-    }
-}
---
+++ Shared/Advent.Of.Code.Shared.Test/TestHelpers/TestFixture.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="TestFixture.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace Advent.Of.Code.Shared.Test.TestHelpers;
+
+/// <summary>
+/// Test Fixture for Advent Of Code puzzles.
+/// </summary>
+public abstract class TestFixture
+{
+    private const string ClassPrefix = "Solver";
+
+    private string InputsDir => Path.Combine("Inputs", $"Day{this.CalculateIndex()}");
+
+    /// <summary>
+    /// Returns the path of the test input file.
+    /// </summary>
+    /// <param name="fileName">Name of the file to get the path of.</param>
+    /// <param name="fileExtension">Extension of the file to get the path of.</param>
+    /// <returns>The path of the test input file.</returns>
+    protected string InputFilePath(string fileName, string fileExtension = "txt") =>
+        Path.Combine(this.InputsDir, $"{fileName}.{fileExtension}");
+
+    private string CalculateIndex()
+    {
+        string typeName = this.GetType().Name;
+
+        string substring = typeName[
+            (typeName.IndexOf(ClassPrefix) + ClassPrefix.Length)..
+        ].TrimStart('_');
+
+        string numbersOnly = new(substring.TakeWhile(char.IsDigit).ToArray());
+
+        uint digits = uint.TryParse(numbersOnly, out uint index) ? index : default;
+
+        return digits.ToString("D2");
+    }
+}
