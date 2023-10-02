e4ed32ac4cb761a8845dd6dedd5fafbdebf82fa3
Jamie Harris <jamiedodgerification12345@gmail.com>
2023-10-02 23:44:10 +0100

    feat(day4): add solutions for year 2022 day 4

---
+++ Solvers/Y2022/Advent.Of.Code.Y2022.Test/Days/Day04/Solver04Tests.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="Solver04Tests.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace Advent.Of.Code.Y2022.Test.Days.Day04;
+
+using Advent.Of.Code.Shared.Test.TestHelpers;
+using Advent.Of.Code.Y2022.Days.Day04;
+
+/// <summary>
+/// Tests for <see cref="Solver04"/>.
+/// </summary>
+public class Solver04Tests : TestFixture
+{
+    private readonly Solver04 solver;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver04Tests"/> class.
+    /// </summary>
+    public Solver04Tests() =>
+        this.solver = new Solver04(inputFilePath: this.InputFilePath("example"));
+
+    /// <summary>
+    /// Tests that <see cref="Solver04.Solve1"/> returns as expected.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Fact]
+    public async Task Solve_1_ReturnsExpected() => (await this.solver.Solve_1()).Should().Be("2");
+
+    /// <summary>
+    /// Tests that <see cref="Solver04.Solve2"/> returns as expected.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Fact]
+    public async Task Solve_2_ReturnsExpected() => (await this.solver.Solve_2()).Should().Be("4");
+}
