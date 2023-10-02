ea6d1d632e527ac6054772a8c71371e3045c79a2
Jamie Harris <jamiedodgerification12345@gmail.com>
2023-10-02 16:44:23 +0100

    feat(setup): setup project (#2)
    * feat(setup): setup project
    
    * feat(setup): setup project
    
    * feat(setup): add 2022 day 1 solutions
    
    * feat(setup): add 2022 day 1 solutions

---
+++ Solvers/Y2022/Advent.Of.Code.Y2022/Days/Day01/Solver01.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="Solver01.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace Advent.Of.Code.Y2022.Days.Day01;
+
+using Advent.Of.Code.Shared.AOCHelper;
+using FileParser;
+
+/// <summary>
+/// Solver for day 1.
+/// </summary>
+public class Solver01 : BaseSolver<long>
+{
+    private readonly IEnumerable<int> groupSums;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver01"/> class.
+    /// </summary>
+    public Solver01()
+        : this(null) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver01"/> class.
+    /// </summary>
+    /// <param name="inputFilePath">The expected input file path.</param>
+    public Solver01(string? inputFilePath)
+        : base(inputFilePath) =>
+        this.groupSums = ParsedFile
+            .ReadAllGroupsOfLines<int>(this.InputFilePath)
+            .ConvertAll(group => group.Sum());
+
+    /// <inheritdoc/>
+    protected override long Solve1() => this.groupSums.Max();
+
+    /// <inheritdoc/>
+    protected override long Solve2() => this.groupSums.OrderDescending().Take(3).Sum();
+}
