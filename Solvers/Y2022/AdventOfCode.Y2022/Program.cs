4870737d9c96da171e449e973242f5ed0bb72071
Jamie Harris <jamiedodgerification12345@gmail.com>
2023-10-03 22:31:24 +0100

    fix(analysis): fix analysis and code style warnings

--- Solvers/Y2022/AdventOfCode.Y2022/Program.cs
+++ Solvers/Y2022/AdventOfCode.Y2022/Program.cs
@@ -8,15 +8,17 @@ using AoCHelper;
 
 if (args.Length == 0)
 {
-    await Solver.SolveLast(options => options.ClearConsole = false);
+    await Solver.SolveLast(options => options.ClearConsole = false).ConfigureAwait(false);
 }
 else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
 {
-    await Solver.SolveAll(options =>
-    {
-        options.ShowConstructorElapsedTime = true;
-        options.ShowTotalElapsedTimePerDay = true;
-    });
+    await Solver
+        .SolveAll(options =>
+        {
+            options.ShowConstructorElapsedTime = true;
+            options.ShowTotalElapsedTimePerDay = true;
+        })
+        .ConfigureAwait(false);
 }
 else
 {
@@ -24,12 +26,14 @@ else
         arg => uint.TryParse(arg, out uint index) ? index : uint.MaxValue
     );
 
-    await Solver.Solve(
-        indices.Where(index => index < uint.MaxValue),
-        options =>
-        {
-            options.ShowConstructorElapsedTime = true;
-            options.ShowTotalElapsedTimePerDay = true;
-        }
-    );
+    await Solver
+        .Solve(
+            indices.Where(index => index < uint.MaxValue),
+            options =>
+            {
+                options.ShowConstructorElapsedTime = true;
+                options.ShowTotalElapsedTimePerDay = true;
+            }
+        )
+        .ConfigureAwait(false);
 }
