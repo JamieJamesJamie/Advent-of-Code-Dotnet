af0adb1a829f3366ac7bb9a4cb176e9e959597c5
Jamie Harris <jamiedodgerification12345@gmail.com>
2023-10-07 12:19:51 +0100

    feat(day2): add day 2 solutions (#13)
    * feat(day2): add day 2 solutions
    
    * chore(gitignore): update ignored input files

---
+++ Solvers/Y2022/AdventOfCode.Y2022.Test/Days/Day02/Solver02Tests.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="Solver02Tests.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2022.Test.Days.Day02;
+
+using AdventOfCode.Y2022.Days.Day02;
+
+/// <summary>
+/// Tests for <see cref="Solver02"/>.
+/// </summary>
+public class Solver02Tests : TestFixture
+{
+    private readonly Solver02 solver;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver02Tests"/> class.
+    /// </summary>
+    public Solver02Tests() =>
+        this.solver = new Solver02(inputFilePath: this.InputFilePath("example"));
+
+    /// <summary>
+    /// Tests that <see cref="Solver02.Solve1"/> returns as expected.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+    [Fact]
+    public async Task Solve_1_ReturnsExpected() => (await this.solver.Solve_1()).Should().Be("15");
+
+    /// <summary>
+    /// Tests that <see cref="Solver02.Solve2"/> returns as expected.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+    [Fact]
+    public async Task Solve_2_ReturnsExpected() => (await this.solver.Solve_2()).Should().Be("12");
+}
