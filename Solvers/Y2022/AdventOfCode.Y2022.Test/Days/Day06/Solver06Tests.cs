c9710bae0a80157448101367f422433664608315
Jamie Harris <jamiedodgerification12345@gmail.com>
2023-11-13 22:49:57 +0000

    feat(year-2022-day-6): implement day 6 (#39)

---
+++ Solvers/Y2022/AdventOfCode.Y2022.Test/Days/Day06/Solver06Tests.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="Solver06Tests.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2022.Test.Days.Day06;
+
+using AdventOfCode.Y2022.Days.Day06;
+
+/// <summary>
+/// Tests for <see cref="Solver06"/>.
+/// </summary>
+public class Solver06Tests : TestFixture
+{
+    /// <summary>
+    /// Tests that <see cref="Solver06.Solve1"/> returns as expected.
+    /// </summary>
+    /// <param name="testFileSuffix">Suffix of the test file.</param>
+    /// <param name="expected">The expected result.</param>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+    [Theory]
+    [InlineData("1", "7")]
+    [InlineData("2", "5")]
+    [InlineData("3", "6")]
+    [InlineData("4", "10")]
+    [InlineData("5", "11")]
+    public async Task Solve_1_ReturnsExpected(string testFileSuffix, string expected) =>
+        (await this.CreateSolver(testFileSuffix).Solve_1()).Should().Be(expected);
+
+    /// <summary>
+    /// Tests that <see cref="Solver06.Solve2"/> returns as expected.
+    /// </summary>
+    /// <param name="testFileSuffix">Suffix of the test file.</param>
+    /// <param name="expected">The expected result.</param>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+    [Theory]
+    [InlineData("1", "19")]
+    [InlineData("2", "23")]
+    [InlineData("3", "23")]
+    [InlineData("4", "29")]
+    [InlineData("5", "26")]
+    public async Task Solve_2_ReturnsExpected(string testFileSuffix, string expected) =>
+        (await this.CreateSolver(testFileSuffix).Solve_2()).Should().Be(expected);
+
+    private Solver06 CreateSolver(string testFileSuffix) =>
+        new(inputFilePath: this.InputFilePath(fileName: $"example{testFileSuffix}"));
+}
---
+++ Solvers/Y2022/AdventOfCode.Y2022/Days/Day06/Solver06.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="Solver06.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2022.Days.Day06;
+
+using System.Collections;
+
+/// <summary>
+/// Solver for day 6.
+/// </summary>
+public class Solver06 : BaseSolver<int>
+{
+    private readonly string input;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver06"/> class.
+    /// </summary>
+    public Solver06()
+        : this(null) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver06"/> class.
+    /// </summary>
+    /// <param name="inputFilePath">The expected input file path.</param>
+    public Solver06(string? inputFilePath)
+        : base(inputFilePath) => this.input = File.ReadAllText(this.InputFilePath);
+
+    /// <inheritdoc/>
+    protected override int Solve1() => this.GetMarker(4);
+
+    /// <inheritdoc/>
+    protected override int Solve2() => this.GetMarker(14);
+
+    /// <summary>
+    /// Returns the marker for the input given the number of distinct characters that there should be to indicate a marker.
+    /// </summary>
+    /// <param name="numDistinctCharacters">The number of distinct characters that there should be to indicate a marker.</param>
+    /// <returns>The number of characters that need to be processed before the marker is detected.</returns>
+    private int GetMarker(int numDistinctCharacters)
+    {
+        Queue window = new(numDistinctCharacters);
+
+        for (int characterIndex = 0; characterIndex <= this.input.Length; characterIndex++)
+        {
+            if (window.Count == numDistinctCharacters)
+            {
+                window.Dequeue();
+            }
+
+            window.Enqueue(this.input[characterIndex]);
+
+            if (window.ToArray().Distinct().Count() == numDistinctCharacters)
+            {
+                return characterIndex + 1;
+            }
+        }
+
+        return -1;
+    }
+}
