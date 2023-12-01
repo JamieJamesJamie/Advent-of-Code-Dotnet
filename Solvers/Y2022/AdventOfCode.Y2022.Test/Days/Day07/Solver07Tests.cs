6a89fa809e5790b1bfe39f807c930e211551deaa
Jamie Harris <jamiedodgerification12345@gmail.com>
2023-12-01 20:25:30 +0000

    feat(year2022): add day 7 solutions

---
+++ Solvers/Y2022/AdventOfCode.Y2022.Test/Days/Day07/Solver07Tests.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="Solver07Tests.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2022.Test.Days.Day07;
+
+using AdventOfCode.Y2022.Days.Day07;
+
+/// <summary>
+/// Tests for <see cref="Solver07"/>.
+/// </summary>
+public class Solver07Tests : TestFixture
+{
+    private readonly Solver07 solver;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver07Tests"/> class.
+    /// </summary>
+    public Solver07Tests() => this.solver = new Solver07(inputFilePath: this.InputFilePath());
+
+    /// <summary>
+    /// Tests that <see cref="Solver07.Solve1"/> returns as expected.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Fact]
+    public async Task Solve_1_ReturnsExpected() =>
+        (await this.solver.Solve_1()).Should().Be("95437");
+
+    /// <summary>
+    /// Tests that <see cref="Solver07.Solve2"/> returns as expected.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Fact]
+    public async Task Solve_2_ReturnsExpected() =>
+        (await this.solver.Solve_2()).Should().Be("24933642");
+}
---
+++ Solvers/Y2022/AdventOfCode.Y2022/Days/Day07/Solver07.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="Solver07.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2022.Days.Day07;
+
+using System.Runtime.CompilerServices;
+using TreeCollections;
+
+/// <summary>
+/// Solver for day 7.
+/// </summary>
+public class Solver07 : BaseSolver<int>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver07"/> class.
+    /// </summary>
+    public Solver07()
+        : this(null) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver07"/> class.
+    /// </summary>
+    /// <param name="inputFilePath">The expected input file path.</param>
+    public Solver07(string? inputFilePath)
+        : base(inputFilePath) { }
+
+    /// <inheritdoc/>
+    protected override int Solve1() => throw new NotImplementedException();
+
+    /// <inheritdoc/>
+    protected override int Solve2() => throw new NotImplementedException();
+
+    private ReadOnlyEntityTreeNode<string, Node> ParseInput()
+    {
+        ReadOnlyEntityTreeNode<string, Node> root = new(node => node.name, new("/"));
+        ReadOnlyEntityTreeNode<string, Node> currentNode = root;
+
+        ParsedFile file = new(this.InputFilePath);
+
+        while (!file.Empty)
+        {
+            List<string> line = file.NextLine().ToList<string>();
+
+            if (line[0] != "$")
+            {
+                ProcessFile();
+            }
+            else if (line[1] == "cd")
+            {
+                currentNode = ChangeDirectory();
+            }
+        }
+
+        return root;
+    }
+
+    private void ProcessFile() { }
+
+    private ReadOnlyEntityTreeNode<string, Node> ChangeDirectory() { }
+
+    private static int SumSize(this ReadOnlyEntityTreeNode<string, Node> node)
+    {
+        return node.Depth;
+    }
+
+    private sealed record Node(string name, int size = 0);
+}
