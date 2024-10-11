35ce14c1859eba916993d11a40199d7066f9ff40
Jamie Harris <jamiedodgerification12345@gmail.com>
2024-10-11 20:10:00 +0100

    refactor(basesolver): refactor base solver classes into separate files

--- Common/AdventOfCode.Common/AOCHelper/BaseSolver{TSolve1,TSolve2}.cs
+++ Common/AdventOfCode.Common/AOCHelper/BaseSolver{TSolve1,TSolve2}.cs
@@ -1,27 +1,10 @@
-﻿// <copyright file="BaseSolver.cs" company="JamieJamesJamie">
+﻿// <copyright file="BaseSolver{TSolve1,TSolve2}.cs" company="JamieJamesJamie">
 // Copyright (c) JamieJamesJamie. All rights reserved.
 // </copyright>
 
 namespace AdventOfCode.Common.AOCHelper;
 
 using AoCHelper;
-
-/// <summary>
-/// Abstract Base Class to solve Advent of Code puzzles.
-/// </summary>
-/// <typeparam name="TSolve">
-/// The return type of the answers for both
-/// part 1 and part 2 of the problem.
-/// </typeparam>
-public abstract class BaseSolver<TSolve> : BaseSolver<TSolve, TSolve>
-{
-    /// <summary>
-    /// Initializes a new instance of the <see cref="BaseSolver{TSolve}"/> class.
-    /// </summary>
-    /// <param name="inputFilePath">The expected input file path.</param>
-    protected BaseSolver(string? inputFilePath)
-        : base(inputFilePath) { }
-}
 
 /// <summary>
 /// Abstract Base Class to solve Advent of Code puzzles.
