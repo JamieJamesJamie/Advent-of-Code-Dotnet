04b15054c31b423cf386c6f801db9a6fa82eb66d
Jamie Harris <jamiedodgerification12345@gmail.com>
2024-10-19 17:06:50 +0100

    feat(y2015): add day 1 solutions

---
+++ Solvers/Y2015/AdventOfCode.Y2015/Days/Day01/Solver01.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="Solver01.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2015.Days.Day01;
+
+/// <summary>
+/// Solver for day 1.
+/// </summary>
+public class Solver01 : BaseSolver<int, ulong?>
+{
+    private readonly IEnumerable<int> input;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver01"/> class.
+    /// </summary>
+    public Solver01()
+        : this(null) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver01"/> class.
+    /// </summary>
+    /// <param name="inputFilePath">The expected input file path.</param>
+    public Solver01(string? inputFilePath)
+        : base(inputFilePath)
+    {
+        Dictionary<char, int> mappings = new() { { '(', 1 }, { ')', -1 } };
+
+        this.input = new ParsedFile(this.InputFilePath)
+            .ToSingleString()
+            .Select(direction => mappings[direction]);
+    }
+
+    /// <inheritdoc/>
+    protected override int Solve1() => this.input.Sum();
+
+    /// <inheritdoc/>
+    protected override ulong? Solve2()
+    {
+        const int basementFloor = -1;
+        int currentFloor = 0;
+
+        foreach (
+            (int position, int floorPosition) in this.input.Select(
+                (element, index) => (index + 1, element)
+            )
+        )
+        {
+            currentFloor += floorPosition;
+
+            if (currentFloor == basementFloor)
+            {
+                return (ulong)position;
+            }
+        }
+
+        return null;
+    }
+}
--- Solvers/Y2023/AdventOfCode.Y2023.Test/Days/Day01/Solver01Tests.cs
+++ Solvers/Y2023/AdventOfCode.Y2023.Test/Days/Day01/Solver01Tests.cs
@@ -17,7 +17,7 @@ public class Solver01Tests : TestFixture
     /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
     [Fact]
     public async Task Solve_1_ReturnsExpected() =>
-        (await new Solver01(inputFilePath: this.InputFilePath(fileName: $"example1")).Solve_1())
+        (await new Solver01(inputFilePath: this.InputFilePath(fileName: "example1")).Solve_1())
             .Should()
             .Be("142");
 
@@ -27,7 +27,7 @@ public class Solver01Tests : TestFixture
     /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
     [Fact]
     public async Task Solve_2_ReturnsExpected() =>
-        (await new Solver01(inputFilePath: this.InputFilePath(fileName: $"example2")).Solve_2())
+        (await new Solver01(inputFilePath: this.InputFilePath(fileName: "example2")).Solve_2())
             .Should()
             .Be("281");
 }
