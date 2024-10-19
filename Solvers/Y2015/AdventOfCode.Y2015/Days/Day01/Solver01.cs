6ba6337fef08031b9e2f81c05abfb89e720696e5
Jamie Harris <jamiedodgerification12345@gmail.com>
2024-10-19 17:40:13 +0100

    feat(y2015): add day 1 solutions (#146)
    * feat(y2015): add day 1 solutions
    
    * chore(pre-commit): apply automatic changes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Solvers/Y2015/AdventOfCode.Y2015/Days/Day01/Solver01.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="Solver01.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2015.Days.Day01;
+
+/// <summary>
+/// Solver for day 1.
+/// </summary>
+public class Solver01 : BaseSolver<int, uint?>
+{
+    private readonly IEnumerable<int> floorIncrements;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver01"/> class.
+    /// </summary>
+    public Solver01()
+        : this(null) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver01"/> class.
+    /// </summary>
+    /// <param name="inputFilePath">The expected input file path.</param>
+    public Solver01(string? inputFilePath)
+        : base(inputFilePath)
+    {
+        Dictionary<char, int> mappings = new() { { '(', 1 }, { ')', -1 } };
+
+        this.floorIncrements = new ParsedFile(this.InputFilePath)
+            .ToSingleString()
+            .Select(direction => mappings[direction]);
+    }
+
+    /// <inheritdoc/>
+    protected override int Solve1() => this.floorIncrements.Sum();
+
+    /// <inheritdoc/>
+    protected override uint? Solve2()
+    {
+        const int basementFloor = -1;
+        int currentFloor = 0;
+
+        foreach (
+            (uint position, int floorIncrement) in this.floorIncrements.Select(
+                (increment, index) => ((uint)index + 1, increment)
+            )
+        )
+        {
+            currentFloor += floorIncrement;
+
+            if (currentFloor == basementFloor)
+            {
+                return position;
+            }
+        }
+
+        return null;
+    }
+}
