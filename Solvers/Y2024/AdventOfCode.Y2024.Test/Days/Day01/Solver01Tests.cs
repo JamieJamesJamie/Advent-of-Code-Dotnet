2c5205bd772e11cdaafb325970c857b825bfb8d4
Jamie Harris <jamiedodgerification12345@gmail.com>
2024-12-01 15:31:25 +0000

    feat(2024): add day 1 solutions

---
+++ Solvers/Y2024/AdventOfCode.Y2024.Test/Days/Day01/Solver01Tests.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Solver01Tests.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2024.Test.Days.Day01;
+
+using AdventOfCode.Y2024.Days.Day01;
+
+/// <summary>
+/// Tests for <see cref="Solver01"/>.
+/// </summary>
+public class Solver01Tests : TestFixture
+{
+    private readonly Solver01 solver;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver01Tests"/> class.
+    /// </summary>
+    public Solver01Tests() => this.solver = new(inputFilePath: this.InputFilePath());
+
+    /// <summary>
+    /// Tests that <see cref="Solver01.Solve1"/> returns as expected.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Fact]
+    public async Task Solve_1_ReturnsExpected() => (await this.solver.Solve_1()).Should().Be("11");
+
+    /// <summary>
+    /// Tests that <see cref="Solver01.Solve2"/> returns as expected.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Fact]
+    public async Task Solve_2_ReturnsExpected() => (await this.solver.Solve_2()).Should().Be("31");
+}
