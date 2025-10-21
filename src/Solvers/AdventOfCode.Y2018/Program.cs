e8b1c0d6725c4aaf1cca3e880e82f15091252787
Jamie Harris <jamiedodgerification12345@gmail.com>
2025-10-21 23:07:23 +0100

    feat(year-2018): add day 1 solutions

---
+++ src/Solvers/AdventOfCode.Y2018/Program.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="Program.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+using AoCHelper;
+
+if (args.Length == 0)
+{
+    await Solver.SolveLast(options => options.ClearConsole = false).ConfigureAwait(false);
+}
+else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
+{
+    await Solver
+        .SolveAll(options =>
+        {
+            options.ShowConstructorElapsedTime = true;
+            options.ShowTotalElapsedTimePerDay = true;
+        })
+        .ConfigureAwait(false);
+}
+else
+{
+    IEnumerable<uint> indices = args.Select(arg =>
+        uint.TryParse(arg, out uint index) ? index : uint.MaxValue
+    );
+
+    await Solver
+        .Solve(
+            indices.Where(index => index < uint.MaxValue),
+            options =>
+            {
+                options.ShowConstructorElapsedTime = true;
+                options.ShowTotalElapsedTimePerDay = true;
+            }
+        )
+        .ConfigureAwait(false);
+}
