d7861c113422077fe7e913ac0b38f0e175dbb7c9
Jamie Harris <jamiedodgerification12345@gmail.com>
2025-11-23 02:29:19 +0000

    feat(year2019): solve day 1 (#310)

---
+++ src/Solvers/AdventOfCode.Y2019/Days/Day01/Solver01.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="Solver01.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2019.Days.Day01;
+
+/// <summary>
+/// Solver for day 1.
+/// </summary>
+internal sealed class Solver01 : BaseSolver<int>
+{
+    private readonly IReadOnlyList<int> masses;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver01"/> class.
+    /// </summary>
+    public Solver01()
+        : this(null) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver01"/> class.
+    /// </summary>
+    /// <param name="inputFilePath">The expected input file path.</param>
+    public Solver01(string? inputFilePath)
+        : base(inputFilePath) =>
+        this.masses =
+            ParsedFile.ReadAllGroupsOfLines<int>(this.InputFilePath).FirstOrDefault() ?? [];
+
+    /// <inheritdoc/>
+    protected override int Solve1() => this.masses.Sum(FuelRequired);
+
+    /// <inheritdoc/>
+    protected override int Solve2() => this.masses.Sum(FuelRequiredPart2);
+
+    private static int FuelRequired(int mass) => (mass / 3) - 2;
+
+    private static int FuelRequiredPart2(int mass)
+    {
+        int fuelRequired = FuelRequired(mass);
+        return fuelRequired <= 0 ? 0 : fuelRequired + FuelRequiredPart2(fuelRequired);
+    }
+}
---
+++ test/Solvers/AdventOfCode.Y2019.Test/Days/Day01/Solver01Tests.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="Solver01Tests.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2019.Test.Days.Day01;
+
+using AdventOfCode.Y2019.Days.Day01;
+
+/// <summary>
+/// Tests for <see cref="Solver01"/>.
+/// </summary>
+public class Solver01Tests : TestFixture
+{
+    /// <summary>
+    /// Tests that <see cref="Solver01.Solve1"/> returns as expected.
+    /// </summary>
+    /// <param name="exampleIndex">Index of the example.</param>
+    /// <param name="expectedSum">The expected sum.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(2, 2)]
+    [InlineData(3, 654)]
+    [InlineData(4, 33583)]
+    [InlineData(5, 34241)]
+    [InlineData(6, 0)]
+    public async Task Solve_1_ReturnsExpected(uint exampleIndex, uint expectedSum) =>
+        (
+            await new Solver01(
+                inputFilePath: this.InputFilePath(fileName: $"example{exampleIndex}")
+            ).Solve_1()
+        )
+            .Should()
+            .Be($"{expectedSum}");
+
+    /// <summary>
+    /// Tests that <see cref="Solver01.Solve2"/> returns as expected.
+    /// </summary>
+    /// <param name="exampleIndex">Index of the example.</param>
+    /// <param name="expectedSum">The expected sum.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(2, 2)]
+    [InlineData(3, 966)]
+    [InlineData(4, 50346)]
+    [InlineData(5, 51316)]
+    [InlineData(6, 0)]
+    public async Task Solve_2_ReturnsExpected(uint exampleIndex, uint expectedSum) =>
+        (
+            await new Solver01(
+                inputFilePath: this.InputFilePath(fileName: $"example{exampleIndex}")
+            ).Solve_2()
+        )
+            .Should()
+            .Be($"{expectedSum}");
+}
