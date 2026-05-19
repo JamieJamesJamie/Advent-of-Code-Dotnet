ed53a439a8f3def5c6be5acc9320f8af3f823137
Jamie Harris <jamiedodgerification12345@gmail.com>
2026-05-19 20:33:14 +0100

    feat(y2015): add day 6

---
+++ src/Solvers/AdventOfCode.Y2015/Days/Day06/Solver06.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Solver06.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2015.Days.Day06;
+
+/// <summary>
+/// Solver for day 6.
+/// </summary>
+internal sealed class Solver06 : BaseSolver<int>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver06"/> class.
+    /// </summary>
+    public Solver06()
+        : this(null) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Solver06"/> class.
+    /// </summary>
+    /// <param name="inputFilePath">The expected input file path.</param>
+    public Solver06(string? inputFilePath)
+        : base(inputFilePath) { }
+
+    protected override int Solve1() => throw new NotImplementedException();
+
+    protected override int Solve2() => throw new NotImplementedException();
+}
---
+++ test/Solvers/AdventOfCode.Y2015.Test/Days/Day06/Solver06Tests.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="Solver06Tests.cs" company="JamieJamesJamie">
+// Copyright (c) JamieJamesJamie. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode.Y2015.Test.Days.Day06;
+
+using AdventOfCode.Y2015.Days.Day06;
+
+public class Solver06Tests : TestFixture
+{
+    [Theory]
+    [InlineData(1, 1_000_000)]
+    [InlineData(2, 1_000)]
+    [InlineData(3, 0)]
+    [InlineData(4, 998_996)]
+    [InlineData(5, 1)]
+    [InlineData(6, 1_000_000)]
+    public async Task Solve_1_ReturnsExpected(uint exampleIndex, uint expectedNumLights) =>
+        (
+            await new Solver06(
+                inputFilePath: this.InputFilePath(fileName: $"example{exampleIndex}")
+            ).Solve_1()
+        )
+            .Should()
+            .Be($"{expectedNumLights}");
+
+    [Theory]
+    [InlineData(1, 1_000_000)]
+    [InlineData(2, 2_000)]
+    [InlineData(3, 0)]
+    [InlineData(4, 1_001_996)]
+    [InlineData(5, 1)]
+    [InlineData(6, 2_000_000)]
+    public async Task Solve_2_ReturnsExpected(uint exampleIndex, uint expectedNumLights) =>
+        (
+            await new Solver06(
+                inputFilePath: this.InputFilePath(fileName: $"example{exampleIndex}")
+            ).Solve_2()
+        )
+            .Should()
+            .Be($"{expectedNumLights}");
+}
